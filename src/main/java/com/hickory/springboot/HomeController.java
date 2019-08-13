86407224e456fc245495c54e9e5304067a10c27c
suresh cherukuri <sureshcherukuri99@gmail.com>
2019-08-12 22:35:29 -0500

    local commit

---
+++ src/main/java/com/hickory/springboot/HomeController.java
@@ -0,0 +1,15 @@
+package com.hickory.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController 
+{	
+	@RequestMapping("/")
+public String home()
+{
+	//System.out.println("Home page requested!");
+		return "index.jsp";
+}
+}
